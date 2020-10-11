e6e5887f1eb970ca20f2a983c91b1642a7c694c9
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-10-11 10:12:13 +0700

    payment: implement SCB API request wrapper with auto authz header

--- pretix_promptpay_scb/payment.py
+++ pretix_promptpay_scb/payment.py
@@ -1,9 +1,83 @@
+import datetime
+import requests
+import uuid
 from collections import OrderedDict
+from typing import Any, Dict
 
 from django import forms
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from pretix.base.payment import BasePaymentProvider
+from pretix.base.cache import ObjectRelatedCache
+
+class ScbPartnerApi():
+    """
+    Provide convenience wrapper around SCB partner API convention.
+    """
+
+    class BussinessError(RuntimeError):
+        def __init__(self, code, description):
+            super().__init__('Bussiness error %d: %s' % (code, description))
+            self.code = code
+            self.description = description
+
+    access_token: Dict[str, Any]
+
+    def __init__(self, base_url: str, app_key: str, app_secret: str, cache: ObjectRelatedCache):
+        self.base_url = base_url
+        self.v1_url = base_url + '/v1'
+
+        self.app_key = app_key
+        self.app_secret = app_secret
+        self.cache = cache
+
+        self.access_token = None
+
+    def post(self, url: str, json: Dict[str, Any], skip_authz=False):
+        """
+            POST to the URL, handle things common to SCB API.
+            skip_authz is used by get_authz_header() itself.
+            Returns 'data' field directly
+        """
+        headers = {
+            'resourceOwnerId': self.app_key,
+            'requestUId': str(uuid.uuid4()), # Yet to find its purpose.
+            'accept-language': 'EN'
+        }
+        if not skip_authz:
+            headers['authorization'] = self.get_authz_header()
+
+        response = requests.post(url=url, json=json, headers=headers).json()
+        status = response['status']
+        if status['code'] != 1000:
+            raise ScbPartnerApi.BussinessError(status['code'], status['description'])
+
+        return response['data']
+
+    def is_access_token_expired(self):
+        now = timezone.now()
+        expiresAt = datetime.datetime.fromtimestamp(self.access_token['expiresAt'])
+        # Expire the token 60 seconds early, so that we don't use expired token
+        return expiresAt - datetime.timedelta(seconds=60) <= now
+
+    def get_authz_header(self):
+        if self.access_token is None:
+            self.access_token = self.cache.get('scb_access_token')
+
+        if self.access_token is None or self.is_access_token_expired():
+            self.access_token = self.post(
+                url=self.v1_url + '/oauth/token',
+                json={
+                    'applicationKey': self.app_key,
+                    'applicationSecret': self.app_secret,
+                },
+                skip_authz=True
+            )
+            self.cache.set('scb_access_token', self.access_token, timeout=self.access_token['expiresIn'])
+
+        return '%s %s' % (self.access_token['tokenType'], self.access_token['accessToken'])
+
 
 class PromptPayScbPaymentProvider(BasePaymentProvider):
     identifier = 'promptpay_scb'
