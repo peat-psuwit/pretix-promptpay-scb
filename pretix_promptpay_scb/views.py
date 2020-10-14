3b5e4621cb10631b1f6f9fa24311a2a906706cc3
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-10-14 20:40:40 +0700

    views: add forgotten import

--- pretix_promptpay_scb/views.py
+++ pretix_promptpay_scb/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.http.response import JsonResponse, Http404, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView, View
