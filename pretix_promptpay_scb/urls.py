71e790114c405f953cbd115974967cc9333dd457
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-10-16 22:39:05 +0700

    urls: add missing $

--- pretix_promptpay_scb/urls.py
+++ pretix_promptpay_scb/urls.py
@@ -6,7 +6,7 @@ from . import views
 event_patterns = [
     url(r'^order/(?P<order>[^/]+)/(?P<secret>[A-Za-z0-9]+)/pay/(?P<payment>[0-9]+)/promptpay_scb_show_qr$',
         views.ShowQrView.as_view(), name='show_qr'),
-    url(r'^order/(?P<order>[^/]+)/(?P<secret>[A-Za-z0-9]+)/pay/(?P<payment>[0-9]+)/promptpay_scb_state',
+    url(r'^order/(?P<order>[^/]+)/(?P<secret>[A-Za-z0-9]+)/pay/(?P<payment>[0-9]+)/promptpay_scb_state$',
         views.PaymentStateView.as_view(), name='payment_state'),
     event_url(r'^_promptpay_scb/callback/(?P<callback_secret>[A-Za-z0-9]+)$',
         views.callback_view, name='callback', require_live=False),
